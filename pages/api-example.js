38e575cf8a3f85bc079fcc6b9fbad34b634633e0
Ben Goshow <ben@fishhook.us>
2021-07-02 12:49:11 -0400

    finally got auth to work, now on to actual API calls

--- pages/api-example.js
+++ pages/api-example.js
@@ -1,17 +1,22 @@
-import Layout from '../components/layout'
+import Layout from '../components/layout';
 
-export default function Page () {
+export default function Page() {
   return (
     <Layout>
       <h1>API Example</h1>
       <p>The examples below show responses from the example API endpoints.</p>
-      <p><em>You must be signed in to see responses.</em></p>
+      <p>
+        <em>You must be signed in to see responses.</em>
+      </p>
       <h2>Session</h2>
       <p>/api/examples/session</p>
-      <iframe src="/api/examples/session"/>
+      <iframe src="/api/examples/session" />
       <h2>JSON Web Token</h2>
       <p>/api/examples/jwt</p>
-      <iframe src="/api/examples/jwt"/>
+      <iframe src="/api/examples/jwt" />
+      <h2>Freshbooks API</h2>
+      <p>/api/examples/freshbooks-client</p>
+      <iframe src="/api/examples/freshbooks-client" />
     </Layout>
-  )
+  );
 }
---
+++ pages/api/examples/freshbooks-client.js
@@ -0,0 +1,26 @@
+import { Client } from '@freshbooks/api';
+import jwt from 'next-auth/jwt';
+
+const secret = process.env.SECRET,
+  clientId = process.env.FRESHBOOKS_CLIENT_ID;
+
+export default async (req, res) => {
+  const token = await jwt.getToken({ req, secret });
+  //res.send(JSON.stringify(token, null, 2));
+  //res.send(token.accessToken);
+
+  const accessToken = token.accessToken;
+  // Instantiate new FreshBooks API client
+  const client = new Client(accessToken, {
+    clientId: clientId,
+  });
+
+  try {
+    // Get the current user
+    const { data } = await client.users.me();
+    res.send(`Hello, Employee ${data.id}`);
+  } catch ({ code, message }) {
+    // Handle error if API call failed
+    console.error(`Error fetching user: ${code} - ${message}`);
+  }
+};
